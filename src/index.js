dd9415e87b53d689f1f1ac3f39f32565b28c2606
Jordan Marinov <jaisor@gmail.com>
2023-12-06 17:29:30 -0800

    WIP - foundation stuff

--- src/index.js
+++ src/index.js
@@ -1,63 +1,95 @@
 import dotenv from 'dotenv'
-import fs from 'fs';
-import express from 'express';
-import * as client from 'prom-client';
-import schedule from 'node-schedule';
-import moment from 'moment';
+import fs from 'fs'
+import { parse as parseYaml } from 'yaml'
+import express from 'express'
+import * as client from 'prom-client'
+import schedule from 'node-schedule'
+import moment from 'moment'
 import * as mqtt from 'mqtt'
+import mqttPattern from 'mqtt-pattern'
 //import { poll } from './poll-metrics.mjs'
 
-dotenv.config();
-const server = express();
+var registeredMetrics = {}
+
+// Config
+dotenv.config()
+
+const configPath = process.env.CONFIG_PATH || '.config'
+const configFile = fs.readFileSync(configPath + '/config.yaml', 'utf8')
+const config = parseYaml(configFile)
+console.log(config)
+
+// Prometheus client
 const register = new client.Registry()
+register.setDefaultLabels(config.global?.labels || {})
+client.collectDefaultMetrics({ register, prefix: config.global?.prefix || '' })
 
-register.setDefaultLabels({
-  app: 'mqtt-json-prometheus-exporter'
-})
-client.collectDefaultMetrics({ register })
+// MQTT client 
+const mqttClient = mqtt.connect(config.mqtt?.url, config.mqtt?.options )
 
-//
-
-const mqttClient = mqtt.connect('mqtt://localhost:1883')
-
-// Connect to the MQTT broker
+// Connect to the MQTT broker and subscribe to relevant topics
 mqttClient.on('connect', function () {
-  console.log('Connected to MQTT broker')
+  console.log(`Connected to MQTT broker at ${config.mqtt?.url}`)
+	var topic = 'home/outside/json'
+	mqttClient.subscribe(topic, (err) => {
+    if (err) {
+			console.error(`Error subscribing to '${topic}'`, err)
+    } else {
+			console.log(`Subscribed to '${topic}'`)
+		}
+  })
 })
 
+mqttClient.on("message", (topic, message) => {
+  // message is Buffer
+  console.log(message.toString())
+})
+
+// House keeping
+function handleExit(signal) {
+  console.log(`Exiting due to signal ${signal}`)
+	schedule.gracefulShutdown().then(() => process.exit(0))
+}
+process.on('SIGINT', handleExit)
+process.on('SIGTERM', handleExit)
+
 //
 
-process.on('SIGINT', function () {
-	schedule.gracefulShutdown()
-		.then(() => process.exit(0))
-});
 
-//
+var m = 'test'
+const metrics = new client.Gauge({
+	name: m,
+	help: `metric ${m}`,
+})
+register.registerMetric(metrics)
+registeredMetrics[m] = metrics
+registeredMetrics[m].set(Number(10))
 
-var registeredMetrics = {};
-
-function runTask(fireDate) {
+async function runTask(fireDate) {
   console.log(moment().format('lll') + ` (${fireDate}) : Running task`)
 }
 
-runTask(moment().format('lll'));
+runTask(moment().format('lll'))
 schedule.scheduleJob("*/1 * * * *", function(fireDate){
-  runTask(fireDate);
-});
+  runTask(fireDate)
+})
+
+// Express server
+const server = express()
 
 // Populate metrics
 server.get('/metrics', async (req, res) => {
 	try {
-		res.set('Content-Type', register.contentType);
-		res.end(await register.metrics());
+		res.set('Content-Type', register.contentType)
+		res.end(await register.metrics())
 	} catch (ex) {
-		console.error(ex);
-		res.status(500).end(ex);
+		console.error(ex)
+		res.status(500).end(ex)
 	}
-});
+})
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 console.log(
 	`Server listening to ${port}, metrics exposed on /metrics endpoint`,
-);
-server.listen(port);
+)
+server.listen(port)
